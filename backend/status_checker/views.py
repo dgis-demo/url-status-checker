4ec3e919ac29dc73182c01f419c75746dfcc562b
oliver <mail@mail.com>
2020-11-07 13:42:33 +0400

    Added async requests 1)Added functions for asynchronous requests via aiohttp library (status_checker/views.py). 2)Removed duplicated code during json parsing in change_status function(status_checker/views.py). 3)Added aiohttp into the Pipfile.

--- backend/status_checker/views.py
+++ backend/status_checker/views.py
@@ -1,5 +1,6 @@
 import json
-import requests
+import asyncio
+import aiohttp
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
@@ -18,23 +19,43 @@ def load_urls(request):
     return JsonResponse({"urls": urls})
 
 
+async def check_url(url, session):
+    try:
+        request_url = str(url.url)
+        async with session.head(request_url, allow_redirects=True) as response:
+            status = response.status
+            url.status_code = status
+    except Exception as e:
+        url.status_code = 404
+    finally:
+        url.save()
+
+
+async def lookup_urls(urls):
+    tasks = list()
+
+    async with aiohttp.ClientSession() as session:
+        for url in urls:
+            task = asyncio.ensure_future(check_url(url, session))
+            tasks.append(task)
+
+        await asyncio.gather(*tasks)
+
+
 def check_status(request):
     urls = Url.objects.filter(user=request.user, check_status=True)
-    for url in urls:
-        try:
-            request_url = str(url.url)
-            res = requests.head(request_url)
-            url.status_code = res.status_code
-        except Exception as e:
-            url.status_code = 404
-        url.save()
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(lookup_urls(urls))
+    loop.close()
     return JsonResponse({"success": 1})
 
 
 @csrf_exempt
 def change_status(request):
-    url_id = json.loads(request.body)['id']
-    status = json.loads(request.body)['check_status']
+    request_body = json.loads(request.body)
+    url_id = request_body.get('id')
+    status = request_body.get('check_status')
     url = Url.objects.filter(id=url_id)[0]
     url.check_status = status
     if status is False:
